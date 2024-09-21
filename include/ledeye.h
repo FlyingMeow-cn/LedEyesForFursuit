84046f3cde1107b57516a007d3079f69008dca12
FlyingMeow <FlyingMeow_cn@outlook.com>
2024-09-21 23:25:52 +0800

    新建ledeye类
    新建文件、定义头文件

---
+++ include/ledeye.h
@@ -0,0 +1,19 @@
+#ifndef LEDEYE_H
+#define LEDEYE_H
+
+#include <Arduino.h>
+#include <FastLED.h>
+
+class LedEye
+{
+private:
+
+
+public:
+    LedEye();       // 构造函数
+    ~LedEye();      // 析构函数
+    void init();
+
+};
+
+#endif // LEDEYE_H
