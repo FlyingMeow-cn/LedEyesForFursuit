3779a2f1c8e13467a8221b372392ca19ae6f4a49
FlyingMeow <FlyingMeow_cn@outlook.com>
2025-01-09 22:23:56 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,12 @@
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif // 判断蓝牙是否启用
 #include "ble_msg_hdl.h"
+#include "voice_msg_hdl.h"
 #include "device_ctrl.h"
 
 BluetoothSerial SerialBT;
 LedEyes ledEyes;
 String ble_helpmsg = "";
-
 
 void setup()
 {
@@ -19,6 +19,7 @@ void setup()
 
     SerialBT.begin("ESP32_飞喵的发光眼设备"); // 启动蓝牙串口并设置蓝牙的名称
     Serial.begin(9600);
+    Serial2.begin(9600);
 
     helpMsgSetup();
     ledEyes.init();
@@ -32,4 +33,22 @@ void loop()
 {
     bleMsgHandler();
     delay(100);
+    voiceMsgHandler();
+    delay(100);
+
+    // String str2 = "";
+    // while (Serial2.available())
+    // {
+    //     char c = Serial2.read();
+    //     str2 += c;
+    // }
+    // if (str2 != "")
+    // {
+    //     // 打印到串口0
+    //     // delay(10);
+    //     Serial.print("Serial2 receive: " + str2);
+    //     str2.clear();
+    // }
+
+
 }
