cee8c5c9bc16a34ba1ef3822337ad74cb15a9f3f
FlyingMeow <FlyingMeow_cn@outlook.com>
2025-05-11 22:13:18 +0800

    整理颜色列表

--- include/ledeyes.h
+++ include/ledeyes.h
@@ -12,7 +12,7 @@
 #include <FastLED.h>
 #include <math.h>
 
-void taskEyesBlinkTrigger(void *pvParameters); 
+void taskEyesBlinkTrigger(void *pvParameters);
 void taskEyesBlink(void *pvParameters); // 眨眼睛任务 传入参数为LedEyes对象
 void taskEyesUpdate(void *pvParameters);
 void taskLedsColorTrans(void *pvParameters);
@@ -23,9 +23,9 @@ CRGB briScale(CRGB color, float scale);
 
 enum EyesBlinkMode
 {
-    BLINK_OFF,          // 关闭眨眼
-    BLINK_ON_CONSTANT,  // 恒定眨眼时间间隔
-    BLINK_ON_RANDOM     // 伪随机时间间隔
+    BLINK_OFF,         // 关闭眨眼
+    BLINK_ON_CONSTANT, // 恒定眨眼时间间隔
+    BLINK_ON_RANDOM    // 伪随机时间间隔
 };
 
 enum ColorShiftMode
@@ -40,39 +40,24 @@ class LedEyes
 private:
     // 内部常量值
     const int FastLED_BRIGNTNESS_INIT = 255; // 0~255
-    const float LED_BRIGHTNESS_INIT = 0.05;     // 0.0~1.0  // 调试用
+    const float LED_BRIGHTNESS_INIT = 0.05;  // 0.0~1.0  // 调试用
     // const float LED_BRIGHTNESS_INIT = 0.5;      // 0.0~1.0
 
-    // const CRGB color24_1 = CRGB(50, 149, 183);  // 湖蓝色
-    // const CRGB color24_2 = CRGB(255, 50, 0);     // 橙色
-    // const CRGB color24_3 = CRGB(0, 255, 50);     // 青绿色
-    // const CRGB color24_4 = CRGB(50, 0, 255);     // 蓝紫色
-    // const CRGB color24_5 = CRGB(255, 255, 0);   // 黄色
-    // const CRGB color24_6 = CRGB(255, 0, 255);   // 紫色
-    // const CRGB color24_7 = CRGB(0, 255, 255);   // 青色
-    // const CRGB color24_8 = CRGB(255, 255, 255); // 白色
-    // const CRGB color24_9 = CRGB(255, 165, 0);   // 橙色
-    // const CRGB color24_10 = CRGB(165, 20, 255); // 紫色
-    // const CRGB color24_11 = CRGB(20, 165, 255); // 和湖蓝色效果差不多
-    // const CRGB color24_12 = CRGB(20, 255, 165); // 和湖蓝色效果差不多
-
-    const CRGB color24_1 = CRGB(50, 149, 183);  // 湖蓝色
-    const CRGB color24_2 = CRGB(38, 122, 253);    
-    const CRGB color24_3 = CRGB(50, 0, 255);   
+    const CRGB color24_1 = CRGB(50, 149, 183);   // 湖蓝色
+    const CRGB color24_2 = CRGB(38, 122, 253);   // 蓝色
+    const CRGB color24_3 = CRGB(50, 0, 255);     // 蓝紫色
     const CRGB color24_4 = CRGB(255, 0, 255);    // 紫色
-    const CRGB color24_5 = CRGB(241, 97, 146);   
-    const CRGB color24_6 = CRGB(230, 57, 54);   
-    const CRGB color24_7 = CRGB(254, 90, 36);   
-    const CRGB color24_8 = CRGB(254, 168, 40); 
-    const CRGB color24_9 = CRGB(253, 239, 89);   
-    const CRGB color24_10 = CRGB(223, 233, 116); 
-    const CRGB color24_11 = CRGB(129, 203, 197); 
-    const CRGB color24_12 = CRGB(130, 223, 237); 
+    const CRGB color24_5 = CRGB(241, 97, 146);   // 粉色
+    const CRGB color24_6 = CRGB(230, 57, 54);    // 红色
+    const CRGB color24_7 = CRGB(254, 90, 36);    // 橙色
+    const CRGB color24_8 = CRGB(254, 168, 40);   // 橙黄色
+    const CRGB color24_9 = CRGB(253, 239, 200);  // 白色
+    const CRGB color24_10 = CRGB(223, 233, 116); // 浅黄色
+    const CRGB color24_11 = CRGB(129, 203, 197); // 绿色
+    const CRGB color24_12 = CRGB(130, 223, 237); // 青色
 
+    const int LED_COLORTRANS_SPEED_INIT = 4; // 颜色变换速度
 
-    const int LED_COLORTRANS_SPEED_INIT = 4;    // 颜色变换速度
-
-    
     // 类的内部变量
 
     // 内部标志变量
@@ -89,34 +74,34 @@ public:
     CRGB leds_l[NUM_LEDS];            // 最后赋值给WS2812的CRGB值
     CRGB leds_r[NUM_LEDS];            // 最后赋值给WS2812的CRGB值
 
-    const CRGB led_CRGBcolor_init = color24_1;        // 初始颜色的RGB值
-    float led_brightness = LED_BRIGHTNESS_INIT; // 当前亮度0.0~1.0
+    const CRGB led_CRGBcolor_init = color24_1;                                                        // 初始颜色的RGB值
+    float led_brightness = LED_BRIGHTNESS_INIT;                                                       // 当前亮度0.0~1.0
     const int eyes_blink_palse_ms[10] = {5000, 3500, 6000, 4000, 7000, 4500, 8000, 5000, 9000, 5500}; // 眨眼睛间隔时间
     // int eyes_blink_palse_ms = 5000; // 眨眼睛间隔时间
-    const int eyes_blink_delay_ms = 20;   // 眨眼睛task CRGB值刷新的间隔时间
-    bool flag_eyes_blink = true;    // 眨眼睛标志位
+    const int eyes_blink_delay_ms = 20;                // 眨眼睛task CRGB值刷新的间隔时间
+    bool flag_eyes_blink = true;                       // 眨眼睛标志位
     EyesBlinkMode eyes_blink_mode = BLINK_ON_CONSTANT; // 眨眼睛标志位
 
     bool flag_eyes_bri_gradient = true; // 眼睛颜色渐变标志位
 
-    bool flag_eyes_colortrans = false; // 眼睛颜色变换标志位
-    int colorTransSpeed = LED_COLORTRANS_SPEED_INIT;          // 颜色变换速度
-    
+    bool flag_eyes_colortrans = false;               // 眼睛颜色变换标志位
+    int colorTransSpeed = LED_COLORTRANS_SPEED_INIT; // 颜色变换速度
+
     const int color_shift_update_pluse_ms = 50;
     int color_shift_mode = COLOR_SHIFT_OFF; // 颜色过渡模式
     // const int COLOR_SHIFT_DELAY_MS_INIT = 1000 * 30; // 颜色切换延时
-    const int COLOR_SHIFT_DELAY_MS_INIT = 1000 * 2; // 颜色切换延时  调试用
+    const int COLOR_SHIFT_DELAY_MS_INIT = 1000 * 2;       // 颜色切换延时  调试用
     int color_shift_delay_ms = COLOR_SHIFT_DELAY_MS_INIT; // 颜色过渡延时
 
     const float bri_seq[8] = {0.03, 0.05, 0.1, 0.2, 0.3, 0.5, 0.7, 1.0};
     const int bri_seq_len = 8;
     const int bri_seq_idx_rst = 5;
-    int bri_seq_idx = bri_seq_idx_rst;   
+    int bri_seq_idx = bri_seq_idx_rst;
 
     const CRGB color24_seq[12] = {color24_1, color24_2, color24_3, color24_4, color24_5, color24_6, color24_7, color24_8, color24_9, color24_10, color24_11, color24_12};
     const int color24_seq_len = 12;
     const int color24_seq_idx_rst = 0;
-    int color_seq_idx = color24_seq_idx_rst; 
+    int color_seq_idx = color24_seq_idx_rst;
     int color_seq_delta_idx = 1; // 颜色序列变化的步长 1或-1
 
     LedEyes();  // 构造函数
